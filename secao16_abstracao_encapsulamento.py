d6932745f87725fae40efa6a9d732294f9d310ea
David Machado <machadodavid32@gmail.com>
2022-07-12 10:31:38 -0300

    Adicionando aula secao 16 - abstração e encapsumento em poo

---
+++ secao16_abstracao_encapsulamento.py
@@ -0,0 +1,89 @@
+"""
+POO - Abstração e encapsulamento
+
+O grande objetivo da programação orientada a objeto é encapsular nosso codigo dentro de um grupo logico e hierarquico
+utilizando classes.
+
+Abstração - Em POO, é o ato de expor apenas dados relevantes de uma classe, escondendo atributos e métodos privados
+do usuário.
+
+
+
+
+
+class Conta:
+    contador = 400
+
+    def __init__(self, titular, saldo, limite):
+        self.numero = Conta.contador
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def extrato(self):
+        print(f'Saldo de {self.saldo} do titular {self.titular} com limite de {self.limite} ')
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
+
+
+# testando
+
+conta1 = Conta('Geek', 150.00, 1500)
+print(conta1.titular)
+print(conta1.numero)
+print(conta1.saldo)
+print(conta1.limite)
+
+# Resposta: Geek
+# 400
+# 150.0
+# 1500
+# No codigo acima, os dados do cliente estão expostos e também podem ser modificados. Exemplo abaixo:
+
+conta1.numero = 42
+conta1.titular = "Xuxa"
+conta1.saldo = 999999999999
+conta1.limite = 99999999999999999999
+print(conta1.__dict__)
+
+# Resposta: {'numero': 42, 'titular': 'Xuxa', 'saldo': 999999999999, 'limite': 99999999999999999999}
+# Reparar que consegui ver e modificar os dados. Isso não pode. Então, vamos tornar os dados mais seguros refatorando..
+# o codigo acima
+
+"""
+
+
+class Conta:
+    contador = 400
+
+    def __init__(self, titular, saldo, limite):
+        self.__numero = Conta.contador
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print(f'Saldo de {self.__saldo} do titular {self.__titular} com limite de {self.__limite} ')
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+
+# testando
+
+conta1 = Conta('Geek', 150.00, 1500)
+print(conta1.__dict__)
+conta1.extrato()
+
+# acima é o codigo considerado seguro. Porém, utilizando o name mangling, podemos ter acesso.
+
+print(conta1._Conta__titular)  # Acesso na safadeza com mangling
+# Resposta: Geek
+
